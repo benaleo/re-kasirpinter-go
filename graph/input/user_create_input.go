606b83fdbd1f04702f95c6de10827deac8172555
benaleo <benaleo.projects@gmail.com>
2026-04-19 15:06:34 +0700

    feat: add optional avatar field to CreateUserInput

--- graph/input/user_create_input.go
+++ graph/input/user_create_input.go
@@ -1,10 +1,11 @@
 package input
 
 type CreateUserInput struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Address  string `json:"address"`
-	Phone    string `json:"phone"`
-	Password string `json:"password"`
-	RoleID   *int64 `json:"role_id,omitempty"`
+	Name     string  `json:"name"`
+	Email    string  `json:"email"`
+	Address  string  `json:"address"`
+	Phone    string  `json:"phone"`
+	Password string  `json:"password"`
+	RoleID   *int64  `json:"role_id,omitempty"`
+	Avatar   *string `json:"avatar,omitempty"`
 }
